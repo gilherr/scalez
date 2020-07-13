a7d6cfa72ea053075f6f3c4a8d94e2cdb2a5dbcd
Gil Herr <gilherr@gmail.com>
2020-07-13 19:02:46 +0300

    config basic eslintrc and some minor changes

--- backend/app/routes/closet.js
+++ backend/app/routes/closet.js
@@ -1,7 +1,7 @@
 const closet = require('express').Router();
 
 closet.get('/', (req, res) => {
-    res.status(200).json({ message: 'not implemented' });;
+    res.status(200).json({ message: 'not implemented' });
 });
 
 module.exports = closet;
