dd1f0516677ff3e2181e3043345b53beb9ee8872
Gil Herr <gilherr@gmail.com>
2020-07-13 19:40:04 +0300

    add user routes

--- backend/app/routes/users.js
+++ backend/app/routes/users.js
@@ -1,7 +1,35 @@
 const users = require('express').Router();
+const db = require('../db/DBService')
 
-users.get('/', (req, res) => {
-    res.status(200).json({ message: 'not implemented' });
+users.get('/', async (req, res) => {
+    try {
+        const rows = await db.getAllUsers();
+        res.status(200).json(rows);
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({error: 'failed to get all users'});
+    }
+});
+
+users.put('/', async (req, res) => {
+    try {
+        await db.createNewUser();
+        res.status(200).send();
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({error: 'failed to create user'});
+    }
+});
+
+users.get('/:userId', async (req, res) => {
+    try {
+        const user = await db.getUser(req.params.userId)
+        res.status(200).json(user);
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({error: 'failed to get user'});
+    }
+
 });
 
 module.exports = users;
