b270516dc4062a58373eb0e7fc1b49213a1cbaa9
Gil Herr <gilherr@gmail.com>
2020-07-16 21:29:27 +0300

    FE: minor updates to Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-view">
-    <img src="../assets/home/home.jpg" alt="Shopping Lady" width="300">
+    <img src="../assets/home/cover.png" alt="Shopping Lady" width="300" />
     <h1>Ready to StyleUp?!</h1>
     <p>I'll show you personalized style advice. You tell me which you like. And i'll show you products that match!</p>
     <router-link to="funnel">Next ></router-link>
