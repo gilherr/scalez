9514839d4d7daa7948951dd8103b940f385eda0d
Gil Herr <gilherr@gmail.com>
2020-07-16 12:23:31 +0300

    FE: add http service to handle http requests

---
+++ frontend/src/services/http.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  withCredentials: true
+})
+
+export const httpService = {
+
+  async fetchAllUsers() {
+    try {
+      const response = await http.get(`/users`);
+      return response.data.sort((a,b)=>a.user_id - b.user_id);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  },
+
+  updateUserMeta(user_id, is_new) {
+    try {
+      http.put(`/users/${user_id}?isNew=${is_new}`);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+}
