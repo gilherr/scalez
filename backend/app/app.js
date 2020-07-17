509a3ff0c46de8e39e9839704fcf622a9aee8fc1
Gil Herr <gilherr@gmail.com>
2020-07-17 23:50:22 +0300

    Misc: fix frontend loading inside the container

--- backend/app/app.js
+++ backend/app/app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || '3000';
 
 // Setup express app
 const app = express();
-app.use(cors({credentials: true, origin: 'http://localhost:8080'}))
+app.use(cors({credentials: true, origin: 'http://localhost:4000'}))
 app.use(cookieParser());
 app.use(customMiddleWares.getUserMetaFromCookie);
 
